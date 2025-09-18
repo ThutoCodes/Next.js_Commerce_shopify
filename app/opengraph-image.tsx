8fb27e6960c9feb6b584c9e983e29c661f2770e7
Thuto Thabang Mpshe <thutompshe132@gmail.com>
2025-09-18 11:56:45 +0200

    Comment out Open Graph and revalidation routes for deployment

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,5 +1,9 @@
+// import OpengraphImage from 'components/opengraph-image';
+
+/*
 import OpengraphImage from 'components/opengraph-image';
 
 export default async function Image() {
   return await OpengraphImage();
 }
+*/
